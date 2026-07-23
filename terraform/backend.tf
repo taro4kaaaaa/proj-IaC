fcd8ebdf1795153462f7fd87b59ddc116418bc70
Konstantin Tarasov <konstantin@Konstantins-MacBook-Pro.local>
2026-07-23 14:54:11 +0300

    добавить backend

---
+++ terraform/backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    bucket = "terraform-state-konstantin"
+    region = "ru-central1"
+    key = "terraform.tfstate"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+  }
+}
